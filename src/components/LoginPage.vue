5e2250e8a36b07667849aeb13c1b943b58aa712e
Steven S. <freelancers2010@gmail.com>
2024-01-18 16:27:16 -0600

    Tailwindcss

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,16 +1,44 @@
 <template>
-  <div>
-    <h1>Login</h1>
-    <form @submit.prevent="login">
-      <label for="username">Username:</label>
-      <input type="text" v-model="username" required />
-      <br />
-      <label for="password">Password:</label>
-      <input type="password" v-model="password" required />
-      <br />
-      <button type="submit">Login</button>
-    </form>
-  </div>
+  <h1>Login Page</h1>
+
+  <form class="max-w-sm mx-auto text-left" @submit.prevent="login">
+    <div class="mb-5">
+      <label
+        for="username"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Username:</label
+      >
+      <input
+        type="text"
+        id="username"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        placeholder="name@flowbite.com"
+        v-model="username"
+        required
+      />
+    </div>
+    <div class="mb-5">
+      <label
+        for="password"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >Password</label
+      >
+      <input
+        type="password"
+        id="password"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        v-model="password"
+        required
+      />
+    </div>
+
+    <button
+      type="submit"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+    >
+      Submit
+    </button>
+  </form>
 </template>
 
 <script>
--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <h1>Welcome, {{ username }}!</h1>
-    <button @click="logout">Logout</button>
+
+    <button
+      type="submit"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+      @click="logout"
+    >
+      Logout
+    </button>
   </div>
 </template>
 
