eb84964ce4c29cdc92ff24943cc0e929e913ee61
Steven S. <freelancers2010@gmail.com>
2024-01-24 11:35:12 -0600

    Updates

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,44 +1,52 @@
 <template>
-  <h1>Login Page</h1>
-
-  <form class="max-w-sm mx-auto text-left" @submit.prevent="login">
-    <div class="mb-5">
-      <label
-        for="username"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >Username:</label
-      >
-      <input
-        type="text"
-        id="username"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        placeholder="name@flowbite.com"
-        v-model="username"
-        required
-      />
-    </div>
-    <div class="mb-5">
-      <label
-        for="password"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-        >Password</label
-      >
-      <input
-        type="password"
-        id="password"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-        v-model="password"
-        required
-      />
-    </div>
-
-    <button
-      type="submit"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+  <div
+    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto"
+  >
+    <h1
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
     >
-      Submit
-    </button>
-  </form>
+      Login Page
+    </h1>
+
+    <form class="max-w-sm mx-auto text-left" @submit.prevent="login">
+      <div class="mb-5">
+        <label
+          for="username"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Username:</label
+        >
+        <input
+          type="text"
+          id="username"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="name@flowbite.com"
+          v-model="username"
+          required
+        />
+      </div>
+      <div class="mb-5">
+        <label
+          for="password"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >Password</label
+        >
+        <input
+          type="password"
+          id="password"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          v-model="password"
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+      >
+        Submit
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h1>Welcome, {{ username }}!</h1>
+  <div
+    class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto"
+  >
+    <h1
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      Welcome, {{ username }}!
+    </h1>
 
     <button
       type="submit"
@@ -9,6 +15,8 @@
     >
       Logout
     </button>
+
+    <pre v-html="JSON.stringify(userInfo, null, 2)"></pre>
   </div>
 </template>
 
@@ -17,6 +25,12 @@ export default {
   data() {
     return {
       username: "",
+    };
+  },
+  setup() {
+    const userInfo = JSON.parse(localStorage.getItem("user"));
+    return {
+      userInfo,
     };
   },
   mounted() {
